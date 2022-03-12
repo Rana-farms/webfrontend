940753b2cdebba83a7f16fc7e0347749b53d2d3f
Xcedeveloper <xceldeveloper@gmail.com>
2022-03-12 16:58:52 +0100

    attention

--- layouts/investor.vue
+++ layouts/investor.vue
@@ -2,31 +2,24 @@
   <v-app id="inspire">
      <investor-sidebar/>
     <investor-header/>
-
-  
-
-    <!-- Sizes your content based upon application components -->
     <v-main>
       <Nuxt />
     </v-main>
-
-    <v-footer       mobile-breakpoint="1024"
- app>
-      footer
-      <!-- -->
-    </v-footer>
   </v-app>
 </template>
 
 <script>
 import InvestorHeader from '~/components/investor/investor-header.vue'
 import InvestorSidebar from '~/components/investor/investor-sidebar.vue'
+import AppFooter from '~/components/views/app-footer.vue'
 export default {
   components: {
     InvestorHeader,
     InvestorSidebar,
+    AppFooter
   },
-  data() {
+  data()
+     {
     return {
       drawer: true,
     }
