5e2f54ff07f047ee323492ad88b0b34e48c94240
Xcedeveloper <xceldeveloper@gmail.com>
2022-04-04 23:23:43 +0100

    notification

--- components/views/notification-alert.vue
+++ components/views/notification-alert.vue
@@ -1,23 +1,46 @@
 <template>
-  <div class="notification">
-      <v-alert type="success">
-      I'm a success alert.
-    </v-alert>
+  <div class="notification" v-if="notification">
+    <v-snackbar
+      :color="notification.color || 'primary'"
+      :timeout="notification.timeout "
+      :right="notification.right"
+      :left="notification.left"
+      :bottom="notification.bottom"
+      :top="notification.top"
+      v-model="snackbar"
+    >
+      {{ notification.message }}
+    </v-snackbar>
   </div>
 </template>
-
 <script>
+import {mapGetters} from 'vuex'
 export default {
-  
+  data() {
+    return {
+      timeout: 3000,
+      snackbar: false,
+    }
+  },
+
+  computed: {
+    ...mapGetters({ alert: 'alert/alert' }),
+
+    notification() {
+      return this.alert
+    },
+  },
+  watch: {
+    notification(val) {
+      if (val) {
+        this.snackbar = true
+
+        setTimeout(() => {
+          this.snackbar = false
+          this.$store.dispatch({ type: 'alert/clear' })
+        }, val?.alert || this.timeout)
+      }
+    },
+  },
 }
 </script>
-
-<style lang="scss" scoped>
-.notification{
-    z-index: 99999999;
-    position: fixed;
-    bottom: 25;
-    right: 25;
-
-}
-</style>
