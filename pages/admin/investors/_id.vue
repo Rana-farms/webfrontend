d549db17a6892dbf846d3841046e2e589be75360
Xcedeveloper <xceldeveloper@gmail.com>
2022-05-28 18:43:29 +0100

    update

--- pages/admin/investors/_id.vue
+++ pages/admin/investors/_id.vue
@@ -2,59 +2,85 @@
   <div class="page">
     <span class="block font-semibold mb-5 text-2xl">User Profile</span>
 
- <v-skeleton-loader v-if="user == null"
-        class="mx-auto"
-        type="card"
-        width="100%"
-        height="160px"
-      ></v-skeleton-loader>
+    <v-skeleton-loader
+      v-if="user == null"
+      class="mx-auto"
+      type="card"
+      width="100%"
+      height="160px"
+    ></v-skeleton-loader>
 
-    <div v-else
-      class="inline-flex gap-10 flex-wrap items-center  mt-5 rounded-lg bg-white shadow  mx-auto  p-10"
+    <div
+      v-else
+      class="inline-flex gap-10 flex-wrap items-center mt-5 rounded-lg bg-white shadow mx-auto p-10"
     >
-    <div>
-        <span class="font-semibold text-3xl block">{{user.fullname}}</span>
-        <span class="text-gray-400 block mb-1">{{user.email}}</span>
-        <span class=" text-green-500 bg-green-100 rounded-lg py-1 px-2" v-if="user.isVerified">Verified</span>
-        <span class=" text-yellow-500 bg-yellow-100 rounded-lg py-1 px-2" v-else>Pending verification</span>
+      <div>
+        <span class="font-semibold text-3xl block">{{ user.fullname }}</span>
+        <span class="text-gray-400 block mb-1">{{ user.email }}</span>
+        <span
+          class="text-green-500 bg-green-100 rounded-lg py-1 px-2"
+          v-if="user.isVerified"
+          >Verified</span
+        >
+        <span class="text-yellow-500 bg-yellow-100 rounded-lg py-1 px-2" v-else
+          >Pending verification</span
+        >
       </div>
 
       <div class="p-5 rounded-lg" style="background-color: #9d1e230d">
         <span class="block text-gray-400 text-sm">Total Investment</span>
-        <span class="text-primary font-semibold text-lg block"
-          >₦12,000,000.00</span
-        >
+        <span class="text-primary font-semibold text-lg block">{{
+          user.totalInvestment | currency
+        }}</span>
       </div>
 
       <div class="p-5 rounded-lg" style="background-color: #f2faf4">
         <span class="block text-gray-400 text-sm">Total Recieved</span>
-        <span class="text-green-500 font-semibold text-lg block"
-          >₦15,400,000.00</span
-        >
+        <span class="text-green-500 font-semibold text-lg block">{{
+          user.totalReceived | currency
+        }}</span>
       </div>
 
       <div class="p-5 rounded-lg" style="background-color: #ffe8e845">
         <span class="block text-gray-400 text-sm">Current Investment</span>
-        <span class="text-yellow-500 font-semibold text-lg block"
-          >₦5,000,000.00</span
-        >
+        <span class="text-yellow-500 font-semibold text-lg block">{{
+          user.currentInvestment | currency
+        }}</span>
       </div>
     </div>
 
-    <!-- <div class="mt-10">
-      <span class="font-semibold block text-lg mb-1">Transaction History</span>
-      <v-data-table :headers="headers" :items="desserts" class="elevation-0">
-        <template v-slot:[`item.date`]="{ item }">
+    <div class="mt-10">
+      <span class="font-semibold block text-lg mb-1"
+        >Investor's Transaction History</span
+      >
+      <v-data-table
+        :headers="transactionsHeader"
+        fixed-header
+        :items="(user && user.transactions) || []"
+        :loading="user == null"
+        class="elevation-0"
+      >
+        <template v-slot:[`item.dateCreated`]="{ item }">
           <div class="inline-flex gap-1">
-            <v-btn color="green" icon v-if="item.type == 'deposit'">
+            <v-btn
+              color="green"
+              icon
+              v-if="item.transaction_type == 'Investment'"
+            >
               <v-icon>mdi-arrow-top-right-thin</v-icon></v-btn
             >
             <v-btn v-else color="red" icon>
               <v-icon>mdi-arrow-bottom-left-thin</v-icon></v-btn
             >
-            <span class="text-gray-600">{{ item.date }}</span>
+            <span class="text-gray-600">{{
+              format(new Date(item.dateCreated), 'MMM do, y ')
+            }}</span>
           </div>
         </template>
+        <template v-slot:[`item.amount`]="{ item }">
+          {{ item.amount | currency }}
+        </template>
+
         <template v-slot:[`item.status`]="{ item }">
           <span
             class="uppercase"
@@ -66,74 +92,102 @@
             >{{ item.status }}</span
           >
         </template>
+
+        <template v-slot:no-data>
+          <div
+            class="w-full flex items-center justify-center h-60"
+            v-if="user && user.transactions.length == 0"
+          >
+            <div class="text-center text-flame">
+              <span class="block text-center">{{ user.fullname }} has not made any transaction</span>
+            </div>
+          </div>
+        </template>
+
+        <template v-slot:loading>
+          <div class="w-full flex items-center justify-center h-72">
+            <div class="text-center text-flame">
+              <v-icon size="40" color="primary"
+                >mdi-format-list-bulleted-square</v-icon
+              >
+              <span class="block mt-2 font-semibold text-center"
+                >Loading transactions...</span
+              >
+            </div>
+          </div>
+        </template>
       </v-data-table>
-    </div> -->
+    </div>
 
     <div class="mt-12">
-       <span class="font-semibold block text-xl mb-2">User Investments</span>
-         <v-data-table
-      :headers="investmentsHeaders"
-      disable-sort
-           :items="user && user.investments || []"
-      :loading="user == null"
-      class="elevation-0"
-    >
-          <template v-slot:no-data>
-        <div
-          class="w-full flex items-center justify-center h-60"
-          v-if="user.investments.length == 0 "
-        >
-          <div class="text-center text-flame">
-            <span class="block text-center"
-              >{{user.fullname}}  has not made any investment</span
-            >
+      <span class="font-semibold block text-xl mb-2"
+        >Investor's Investments</span
+      >
+      <v-data-table
+        :headers="investmentsHeaders"
+        fixed-header
+        disable-sort
+        :items="(user && user.investments) || []"
+        :loading="user == null"
+        class="elevation-0"
+      >
+        <template v-slot:no-data>
+          <div
+            class="w-full flex items-center justify-center h-60"
+            v-if="user.investments.length == 0"
+          >
+            <div class="text-center text-flame">
+              <span class="block text-center"
+                >{{ user.fullname }} has not made any investment</span
+              >
+            </div>
           </div>
-        </div>
+        </template>
 
-      </template>
-
-      <template v-slot:loading>
-        <div class="w-full flex items-center justify-center h-72">
-          <div class="text-center text-flame">
-            <v-icon size="40" color="primary"
-              >mdi-format-list-bulleted-square</v-icon
-            >
-            <span class="block mt-2 font-semibold text-center"
-              >Loading investments...</span
-            >
+        <template v-slot:loading>
+          <div class="w-full flex items-center justify-center h-72">
+            <div class="text-center text-flame">
+              <v-icon size="40" color="primary"
+                >mdi-format-list-bulleted-square</v-icon
+              >
+              <span class="block mt-2 font-semibold text-center"
+                >Loading investments...</span
+              >
+            </div>
           </div>
-        </div>
-      </template>
+        </template>
 
-      <template v-slot:[`item.units`]="{ item }">
-        {{ Intl.NumberFormat().format(item.units) }}
-      </template>
+        <template v-slot:[`item.units`]="{ item }">
+          {{ Intl.NumberFormat().format(item.units) }}
+        </template>
 
         <template v-slot:[`item.amount`]="{ item }">
-        {{ item.amount | currency }}
-      </template>
+          {{ item.amount | currency }}
+        </template>
 
-      <template v-slot:[`item.status`]="{ item }">
-        <span
-          class="uppercase"
-          :class="{
-            'text-green-500': item.status.toLowerCase() == 'active',
-            'text-red-500': item.status.toLowerCase() == 'inactive',
-          }"
-          >{{ item.status }}</span
-        >
-      </template>
-    </v-data-table>
+        <template v-slot:[`item.status`]="{ item }">
+          <span
+            class="uppercase"
+            :class="{
+              'text-green-500': item.status.toLowerCase() == 'active',
+              'text-red-500': item.status.toLowerCase() == 'inactive',
+            }"
+            >{{ item.status }}</span
+          >
+        </template>
+      </v-data-table>
     </div>
   </div>
 </template>
 
 <script>
+import { format } from 'date-fns'
 export default {
   layout: 'admin',
   data() {
     return {
-          investmentsHeaders: [
+      format: format,
+      investmentsHeaders: [
         {
           text: 'INVESTMENT NAME',
           align: 'start',
@@ -147,68 +201,43 @@ export default {
         { text: ' STATUS', value: 'status' },
       ],
 
-      headers: [
-        { text: 'DATE', value: 'date' },
-        { text: 'DESCRIPTION', value: 'description' },
+      transactionsHeader: [
+        { text: 'DATE', value: 'dateCreated' },
+        { text: 'DESCRIPTION', value: 'transaction_type' },
         { text: 'AMOUNT', value: 'amount' },
-        { text: 'CHARGE', value: 'charge' },
         { text: 'STATUS', value: 'status' },
       ],
-      desserts: [
-        {
-          date: '12 oct. 2021, 07:29PM',
-          description: 'withdraw to bank',
-          type: 'withdraw',
-          charge: '₦3,000,000.00',
-          amount: '₦3,000,000.00',
-          status: 'pending',
-        },
-        {
-          date: '12 oct. 2021, 07:29PM',
-          description: 'Deposit',
-          type: 'deposit',
-          charge: '₦3,000,000.00',
-          amount: '₦3,000,000.00',
-          status: 'success',
-        },
-        {
-          date: '12 oct. 2021, 07:29PM',
-          description: 'withdraw to bank',
-          type: 'withdraw',
-          charge: '₦3,000,000.00',
-          amount: '₦3,000,000.00',
-          status: 'failed',
-        },
-      ],
 
-      user:null,
+      user: null,
     }
   },
-  mounted(){
-   this.getUser();
+  mounted() {
+    this.getUser()
   },
   methods: {
-    async getUser(){
-     try{
-       const {data} = await this.$API.user.fetchUserById(this.$route.params.id)
+    async getUser() {
+      try {
+        const { data } = await this.$API.user.fetchUserById(
+          this.$route.params.id
+        )
         this.user = data.data
-     }catch(error){
-         this.$store.dispatch('alert/setAlert', {
+      } catch (error) {
+        this.$store.dispatch('alert/setAlert', {
           message: error.msg,
           color: 'error',
         })
-     }
-    }
+      }
+    },
   },
-  watch:{
-    user:{
-      handler(user){
-        console.log(JSON.stringify(user, null, 2))
+  watch: {
+    user: {
+      handler(user) {
+        //console.log(JSON.stringify(user, null, 2))
       },
       immediate: true,
-      deep:true
-    }
-  }
+      deep: true,
+    },
+  },
 }
 </script>
 
--- pages/admin/settings/members.vue
+++ pages/admin/settings/members.vue
@@ -23,6 +23,7 @@
       :items="members"
       :loading="isLoadingMembers"
       class="elevation-0"
+      height="300px"
     >
       <template v-slot:[`item.action`]="{ item }">
         <v-menu open-on-hover offset-y bottom max-width="94vw">
@@ -34,7 +35,7 @@
           <div class="rounded" style="width: 250px">
             <v-list elevation="1">
               <v-list-item-group color="primary">
-                <v-list-item @click="changeRole(item.id)">
+                <v-list-item @click="initiateChangeRole(item)">
                   <v-list-item-content>
                     <v-list-item-title> Change Role</v-list-item-title>
                   </v-list-item-content>
@@ -50,43 +51,90 @@
           </div>
         </v-menu>
       </template>
+
+      <template v-slot:loading>
+        <div class="w-full flex items-center justify-center h-72">
+          <div class="text-center text-flame">
+            <v-icon size="40" color="primary"
+              >mdi-format-list-bulleted-square</v-icon
+            >
+            <span class="block mt-2 font-semibold text-center"
+              >Loading Admins...</span
+            >
+          </div>
+        </div>
+      </template>
+
+      <template v-slot:no-data>
+        <div
+          class="w-full flex items-center justify-center h-60"
+          v-if="!isLoadingMembers && members.length == 0 && !errorLoading"
+        >
+          <div class="text-center text-flame">
+            <span class="block text-center">No Admin</span>
+          </div>
+        </div>
+
+        <div
+          class="w-full flex items-center justify-center h-60"
+          v-if="!isLoadingMembers && errorLoading"
+        >
+          <div class="text-center text-flame">
+            <v-icon size="50" color="primary"
+              >mdi-format-list-bulleted-square</v-icon
+            >
+            <span class="block text-center">Error loading admins...</span>
+            <v-btn color="primary" text @click="getAllAdmins">
+              <v-icon left>mdi-refresh</v-icon> Retry</v-btn
+            >
+          </div>
+        </div>
+      </template>
     </v-data-table>
 
     <v-dialog
       v-model="changeRoleDialog"
-      v-if="changeIndex != -1"
+      :persistent="isManagingRole"
       max-width="400px"
       transition="dialog-transition"
     >
       <v-card>
         <div class="p-5 rounded">
           <span class="block font-semibold text-lg text-center">
-            Editing Role</span
+            Manage Admin Role</span
           >
 
           <v-select
             block
             placeholder="Select Role"
-            v-model="members[changeIndex].role"
+            v-model="newRole"
             label="Role"
             class="mt-3 mb-6 pa-0"
             hide-details="auto"
             :items="roles"
-            item-key="name"
+            item-key="value"
             item-text="text"
             outlined
           />
 
           <div class="flex justify-between">
-            <v-btn color="error" elevation="0">Cancel</v-btn>
+            <v-btn color="error" @click="changeRoleDialog = false" elevation="0" v-if="!isManagingRole"
+              >Cancel</v-btn
+            >
 
-            <v-btn color="success" elevation="0">Remove</v-btn>
+            <v-btn
+              color="success"
+              elevation="0"
+              @click="changeRole"
+              :loading="isManagingRole"
+              >Procced</v-btn
+            >
           </div>
         </div>
       </v-card>
     </v-dialog>
 
-<!-- Invite Dialog -->
+    <!-- Invite Dialog -->
     <v-dialog
       v-model="inviteDialog"
       max-width="500px"
@@ -138,9 +186,9 @@ export default {
     return {
       inviteDialog: false,
       roles: [
-        { text: 'Investor', value: 'investor' },
+        // { text: 'Investor', value: 'investor' },
         { text: 'Admin', value: 'admin' },
-        { text: 'Super Admin', value: 'superadmin' },
+        { text: 'Super Admin', value: 'super-admin' },
       ],
       headers: [
         {
@@ -151,46 +199,72 @@ export default {
         },
         { text: 'Email', value: 'email' },
         { text: 'Phone Number', value: 'phone' },
-        { text: 'Role', value: 'role' },
+        { text: 'Role', value: 'role.name' },
         { text: 'Actions', value: 'action' },
       ],
-      members: [
-        {
-          id: 0,
-          fullname: 'John Doe',
-          email: 'johndoe@gmail.com',
-          phone: '09037889592',
-          status: 'active',
-          role: 'Super Admin',
-        },
-        {
-          id: 1,
-          fullname: 'John Doe',
-          email: 'johndoe@gmail.com',
-          phone: '09037889592',
-          status: 'active',
-          role: 'Admin',
-        },
-        {
-          id: 2,
-          fullname: 'John Doe',
-          email: 'johndoe@gmail.com',
-          phone: '09037889592',
-          status: 'active',
-          role: 'Admin',
-        },
-      ],
+      members: [],
       isLoadingMembers: false,
       changeRoleDialog: false,
-      changeIndex: -1,
+      errorLoading: false,
+      selectedMember: null,
+      newRole: null,
+      isManagingRole: false,
     }
+  },
+  mounted() {
+    this.getAllAdmins()
   },
 
   methods: {
-    changeRole(id) {
+    async getAllAdmins() {
+      this.isLoadingMembers = true
+      this.errorLoading = false
+      try {
+        const { data } = await this.$API.admin.fetchAllAdmins()
+        this.members = data.data
+      } catch (error) {
+        this.$store.dispatch('alert/setAlert', {
+          message: error.msg,
+          color: 'error',
+        })
+        this.errorLoading = true
+      } finally {
+        this.isLoadingMembers = false
+      }
+    },
+
+    initiateChangeRole(admin) {
       this.changeRoleDialog = true
 
-      this.changeIndex = this.members.findIndex((member) => member.id === id)
+      this.selectedMember = admin
+      this.newRole =
+        this.roles.find(
+          (role) => role.text.toLowerCase() === admin.role.name.toLowerCase()
+        )?.value ?? ''
+    },
+
+    async changeRole() {
+      try {
+        this.isManagingRole = true
+        await this.$API.admin.manageAdminRole({
+          user_id: this.selectedMember.id,
+          role: this.newRole,
+        })
+        this.$store.dispatch('alert/setAlert', {
+          message: 'Admin Role Updated!',
+          color: 'success',
+        })
+
+        await this.getAllAdmins()
+        this.changeRoleDialog = false
+      } catch (error) {
+        this.$store.dispatch('alert/setAlert', {
+          message: error.msg,
+          color: 'error',
+        })
+      } finally {
+        this.isManagingRole = false
+      }
     },
 
     removeMember(id) {},
